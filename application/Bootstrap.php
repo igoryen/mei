e1795d5e5ca98abe95bb25216e7de219929fd096
igoryen <igor.e@advertiseit.com>
2015-06-24 14:00:09 -0400

    Bootstrap.php: added _initDoctype()

--- application/Bootstrap.php
+++ application/Bootstrap.php
@@ -1,8 +1,9 @@
 <?php
 
-class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
-{
+class Bootstrap extends Zend_Application_Bootstrap_Bootstrap {
 
+  protected function _initDoctype() {
+    
+  }
 
 }
-
