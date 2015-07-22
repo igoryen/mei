33291505095624f256ff802e60b84217adf55b05
igoryen <igor.e@advertiseit.com>
2015-07-22 10:54:38 -0400

    July 22, 2015

--- sql_scripts/insert/in_piece.sql
+++ sql_scripts/insert/in_piece.sql
@@ -5,11 +5,11 @@ SELECT * FROM mei.piece ORDER BY id DESC;
 #============================================================
 
 INSERT INTO mei.piece (week, name, dev_url, git_url, live_url, design_url) VALUES( 
-1528,
-'minecraft_fr',
-'http://dev-offers3.mowinisto.com/redirect/au/minecraft_fr/',
-'',
-'',
-'file:///C:/ig/FR_Minecraft/FR_Minecraft/index.html'
+1529, # week
+'GamePlan', # name
+'http://gameplan.app/', # dev_url
+'https://github.com/GamePlan/www/commits/master', # git_url
+'', # live_url
+'' # design_url
 );
 
